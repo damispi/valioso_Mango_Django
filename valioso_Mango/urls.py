85ba835d84ba842101a9a4bb383121f8bd816f91
damispi <danielspinella@hotmail.com>
2023-11-11 23:05:55 -0300

    Creacion de BD

--- valioso_Mango/urls.py
+++ valioso_Mango/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from valioso_Mapp.views import inicio
+from django.urls import path,include
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',inicio),
+    path('', include ('valioso_Mapp.urls'))
+
 ]
--- valioso_Mapp/models.py
+++ valioso_Mapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Producto(models.Model):
+    titulo=models.CharField(max_length=50,help_text="Ingrese un Título para producto ")
+    descripcion=models.CharField(max_length=200,help_text="Ingrese una descripcion del producto")
+    foto1=models.ImageField(upload_to='valioso_Mapp')
+    foto2=models.ImageField(upload_to='valioso_Mapp')
+    foto4=models.ImageField(upload_to='valioso_Mapp')
+    create=models.DateTimeField(auto_now=True)
+    update=models.DateTimeField(auto_now=True)
 # Create your models here.
