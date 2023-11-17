2fa1ee1f8236d76cc066ddb7a9c2b2179a71b643
Lucas Igarzabal <igarzaballucas@hotmail.com>
2023-11-17 18:44:12 -0300

    hice algo de la herencia para el footer y el video

--- core/urls.py
+++ core/urls.py
@@ -4,8 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.inicio, name="inicio"),
+    path("inicio", views.inicio, name="inicio"),
     path("valioso_mango/Pages/ComoFunciona/", views.ComoFunciona, name="como_funciona"),
     path("ingreso", views.ingreso, name="ingreso"),
     path("registro", views.registro, name="registro"),
-    path("<int:size>", views.video, name="video")
+    path("<int:size>", views.video, name="video"),
 ]
