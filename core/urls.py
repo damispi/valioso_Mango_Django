046ae75a529bba15aa71e96e181f9e9496793ef7
Lucas Igarzabal <igarzaballucas@hotmail.com>
2023-11-19 12:50:17 -0300

    pase los nombres de camelCase a snake_case

--- core/urls.py
+++ core/urls.py
@@ -5,9 +5,11 @@ from . import views
 urlpatterns = [
     path("", views.inicio, name="inicio"),
     path("inicio", views.inicio, name="inicio"),
-    path("valioso_mango/Pages/ComoFunciona/", views.ComoFunciona, name="como_funciona"),
+    path(
+        "valioso_mango/Pages/como_funciona/", views.como_funciona, name="como_funciona"
+    ),
     path("ingreso", views.ingreso, name="ingreso"),
     path("registro", views.registro, name="registro"),
     path("<int:size>", views.video, name="video"),
-    path("mi_tienda", views.mi_tienda, name="mi_tienda")
+    path("mi_tienda", views.mi_tienda, name="mi_tienda"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,28 +4,28 @@ from django.http import HttpResponse
 
 
 def inicio(request):
-    return render(request, "core/Pages/get_size.html")
+    return render(request, "core/pages/get_size.html")
 
 
-def ComoFunciona(request):
-    return render(request, "core/Pages/ComoFunciona.html")
+def como_funciona(request):
+    return render(request, "core/pages/como_funciona.html")
 
 
 def ingreso(request):
-    return render(request, "core/Pages/form_ingreso.html")
+    return render(request, "core/pages/form_ingreso.html")
 
 
 def registro(request):
-    return render(request, "core/Pages/form_registro_usuario.html")
+    return render(request, "core/pages/form_registro_usuario.html")
 
 
 def video(request, size):
     context = {"small": 768, "med": 1024, "size": size}
-    return render(request, "core/Pages/template_video_index.html", context)
+    return render(request, "core/pages/template_video_index.html", context)
 
 
 def mi_tienda(request):
-    return render(request, "core/Pages/MiTienda.html")
+    return render(request, "core/pages/mi_tienda.html")
 
 
 # Create your views here.
